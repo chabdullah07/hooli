64c5b809e74abc227d87ec071ce063a5d9410f32
KaramatSubhani <45454150+KaramatSubhani@users.noreply.github.com>
2023-02-03 17:08:11 +0500

    Login Page added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Drawer from "./components/Drawer";
+import Login from "./components/Login";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Drawer />
+      <Login/>
     </div>
   );
 }
